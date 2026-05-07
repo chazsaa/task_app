b46f9721863b2721a2dd2a329884578bdc3db01e
chazsaa <delavictoria.chariemae08@gmail.com>
2026-05-07 17:33:43 +0800

    Laravel Deployment in Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,27 @@
 <?php
-namespace App \ Providers;
 
-use Illuminate \ Support \ ServiceProvider;
-use Illuminate \ Support \ Facades \ URL;
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * * Register any application services.
-     * */
-    
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
+
     /**
-     * * Bootstrap any application services.
-     * */
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        if (app() - >environment('production')) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
     }
 }
-
